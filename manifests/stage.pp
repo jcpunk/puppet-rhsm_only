1fed18419cfc54135fc241bb0f6305688a4001b2
Pat Riehecky <riehecky@fnal.gov>
2016-03-22 10:11:56 -0500

    Not all puppets set path for unless

--- manifests/stage.pp
+++ manifests/stage.pp
@@ -22,7 +22,7 @@ class rhsm_only::stage {
   if $::rhsm_only::repodir_immutable {
     exec {"chattr +i ${::rhsm_only::repodir}":
       path    => '/usr/bin/:/bin/:/sbin:/usr/sbin',
-      unless  => "lsattr -d ${::rhsm_only::repodir} | sed -e 's/-/:/g' | grep '::i::'",
+      unless  => "/usr/bin/lsattr -d ${::rhsm_only::repodir} | /bin/sed -e 's/-/:/g' | /bin/grep '::i::'",
       require => File[$::rhsm_only::repodir],
     }
   }
