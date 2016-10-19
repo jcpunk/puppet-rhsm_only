abbf97ef70b72bfa563c15a94eec4a0fee32b973
Pat Riehecky <riehecky@fnal.gov>
2016-10-19 09:11:48 -0500

    Added hooks for release file so that you can update to new releases

--- manifests/defaults.pp
+++ manifests/defaults.pp
@@ -2,7 +2,7 @@
 class rhsm_only::defaults {
   $before_packages = true
 
-  case $::osfamily {
+  case $::operatingsystem {
     'RedHat', 'CentOS', 'Scientific', 'Fedora': {
       $repodir           = '/etc/yum.repos.d'
       $rhsm_repofile     = 'redhat.repo'
@@ -11,6 +11,19 @@ class rhsm_only::defaults {
     default: {
       fail("${::operatingsystem} not supported")
     }
+  }
+
+  $manage_release_rpm = true
+  $manage_yum_rpm     = true
+
+  $release_rpm = $::operatingsystem ? {
+    /RedHat/     => 'redhat-release-server',
+    /CentOS/     => 'centos-release',
+    /Scientific/ => $::lsbdistid ? {
+                        /ScientificFermi/ => 'slf-release',
+                        /Scientific/      => 'sl-release',
+                      },
+    /Fedora/     => 'fedora-release',
   }
 
   $certs_dir   = '/etc/pki/entitlement'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,13 +1,16 @@
 # == Class: rhsm_only
 class rhsm_only (
-  $repodir           = $::rhsm_only::defaults::repodir,
-  $rhsm_repofile     = $::rhsm_only::defaults::rhsm_repofile,
-  $repodir_immutable = $::rhsm_only::defaults::repodir_immutable,
-  $certs_dir         = $::rhsm_only::defaults::certs_dir,
-  $certs_mode        = $::rhsm_only::defaults::certs_mode,
-  $certs_owner       = $::rhsm_only::defaults::certs_owner,
-  $certs_group       = $::rhsm_only::defaults::certs_group,
-  $before_packages   = $::rhsm_only::defaults::before_packages,
+  $repodir            = $::rhsm_only::defaults::repodir,
+  $rhsm_repofile      = $::rhsm_only::defaults::rhsm_repofile,
+  $repodir_immutable  = $::rhsm_only::defaults::repodir_immutable,
+  $certs_dir          = $::rhsm_only::defaults::certs_dir,
+  $certs_mode         = $::rhsm_only::defaults::certs_mode,
+  $certs_owner        = $::rhsm_only::defaults::certs_owner,
+  $certs_group        = $::rhsm_only::defaults::certs_group,
+  $before_packages    = $::rhsm_only::defaults::before_packages,
+  $manage_yum_rpm     = $::rhsm_only::defaults::manage_yum_rpm,
+  $manage_release_rpm = $::rhsm_only::defaults::manage_release_rpm,
+  $release_rpm        = $::rhsm_only::defaults::release_rpm,
 
 ) inherits ::rhsm_only::defaults {
 
@@ -19,6 +22,9 @@ class rhsm_only (
   validate_string($certs_owner)
   validate_string($certs_group)
   validate_bool($before_packages)
+  validate_bool($manage_yum_rpm)
+  validate_bool($manage_release_rpm)
+  validate_string($release_rpm)
 
   if $before_packages {
     Package {
@@ -56,12 +62,44 @@ class rhsm_only (
 
   if $repodir_immutable {
     exec {"chattr +i ${repodir}":
-      path    => '/usr/bin/:/bin/:/sbin:/usr/sbin',
-      unless  => "/usr/bin/lsattr -d ${repodir} | /bin/sed -e 's/-/:/g' | /bin/grep '::i::'",
-      require => File[$repodir],
+      path   => '/usr/bin/:/bin/:/sbin:/usr/sbin',
+      unless => "/usr/bin/lsattr -d ${repodir} | /bin/sed -e 's/-/:/g' | /bin/grep '::i::'",
+      before => File[$repodir],
     }
   }
 
+  if $manage_yum_rpm {
+    exec {'yum -y update yum':
+      path   => '/usr/bin/:/bin/:/sbin:/usr/sbin',
+      onlyif => "yum list updates yum && chattr -i ${repodir}",
+    }
+    if $repodir_immutable {
+      Exec['yum -y update yum'] {
+        notify => Exec["chattr +i ${repodir}"],
+        before => [ Exec["chattr +i ${repodir}"], File[$repodir],],
+      }
+    } else {
+      Exec['yum -y update yum'] {
+        before => File[$repodir],
+      }
+    }
+  }
 
+  if $manage_release_rpm {
+    exec {"yum -y update ${release_rpm}":
+      path   => '/usr/bin/:/bin/:/sbin:/usr/sbin',
+      onlyif => "yum list updates ${release_rpm} && chattr -i ${repodir}",
+    }
+    if $repodir_immutable {
+      Exec["yum -y update ${release_rpm}"] {
+        notify => Exec["chattr +i ${repodir}"],
+        before => [ Exec["chattr +i ${repodir}"], File[$repodir],],
+      }
+    } else {
+      Exec["yum -y update ${release_rpm}"] {
+        before => File[$repodir],
+      }
+    }
+  }
 
 }
